1b1256e9aa114e9fbedff2426bfb9f7b0d17f162
MariiaLuk <mashkenciy@gmail.com>
2023-01-05 09:38:12 -0500

    Task1 is completed

--- src/week1/Task1.java
+++ src/week1/Task1.java
@@ -1,4 +1,29 @@
 package week1;
 
 public class Task1 {
+    public static void main(String[] args) {
+
+        System.out.println(identify(5));
+        System.out.println(identify(6));
+
+    }
+
+    public static String identify(int number){
+        if(number %2 == 0){
+            return "Even";
+        }
+        return "Odd";
+    }
+
 }
+
+
+
+/*
+write a method which can identify given number is even or odd
+
+ex
+
+identify(5) -> "Odd"
+identify(6) -> "Even"
+ */
