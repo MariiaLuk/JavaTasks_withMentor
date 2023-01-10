e831c2dadc3aa7ff7bdaab9eae26e1f59161174c
Mariia_Cydeo <mashkenciy@gmail.com>
2023-01-10 17:06:21 -0500

    Task1 is completed

--- src/week2/Task1.java
+++ src/week2/Task1.java
@@ -1,4 +1,27 @@
 package week2;
 
 public class Task1 {
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 200;
+
+        System.out.println("Before swapping: a = "+ a+", b = "+ b);
+
+        b = a + b; // 100 + 200 = 300
+        a = b - a; // 300 - 100 = [a = 200]
+        b = b - a; // 300 - 200 = [b = 100]
+
+        System.out.println("After swapping: a = "+ a+", b = "+ b);
+
+
+
+    }
+
 }
+
+
+/*
+swap two variable values without using a third value
+
+ */
