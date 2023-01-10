e2851e60e490de21aa8e7e17332708e892062fbc
Mariia_Cydeo <mashkenciy@gmail.com>
2023-01-10 17:40:16 -0500

    Task3 has been completed

--- src/week2/Task3.java
+++ src/week2/Task3.java
@@ -1,4 +1,33 @@
 package week2;
 
 public class Task3 {
+
+    public static void main(String[] args) {
+
+        printNumbers(24);
+
+    }
+
+    public static void printNumbers( int n ){
+        String result = "";
+        for (int i = 1; i <= n; i++) {
+            if( i % 2== 0 || i % 3 == 0 || i % 5 ==0){
+                if(i % 2 == 0){
+                    result +="Codility";
+                }
+                if(i % 3 == 0){
+                    result += "Test";
+                }
+                if(i % 5 == 0){
+                    result += "Coders";
+                }
+            }else{
+                result += "" + i;
+            }
+            result += "\n";
+        }
+        System.out.println(result);
+    }
+
 }
+
