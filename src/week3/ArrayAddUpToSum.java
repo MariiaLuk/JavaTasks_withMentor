48f223b94d7d3f8242e41dccdd9841a0a5d55626
Mariia_Cydeo <mashkenciy@gmail.com>
2023-01-16 12:02:35 -0500

    Array Add Up to sum task is completed

---
+++ src/week3/ArrayAddUpToSum.java
@@ -0,0 +1,35 @@
+package week3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayAddUpToSum {
+    public static void main(String[] args) {
+       int[] arr = {8, 7, 2, 5, 3, 1};
+       int sum = 10;
+        Map<Integer,Integer> pairs = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == sum) {
+                    pairs.put(arr[i],arr[j]);
+                }
+            }
+        }
+        System.out.println(pairs);
+
+
+    }
+}
+
+
+/*
+3. Array Add up to sum
+given an int[] 'arr' and another int 'sum',
+     Write a function which can find a pair of ints in 'arr' that add up to 'sum'.
+        Example:
+            arr = [8, 7, 2, 5, 3, 1]
+            sum = 10
+            Output:
+                {8=2, 7=3}
+ */
