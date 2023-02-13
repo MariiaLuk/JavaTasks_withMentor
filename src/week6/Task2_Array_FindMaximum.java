10b4489cb5bfa5c32b40e86b8b7f7f64b179313a
Mariia_Cydeo <mashkenciy@gmail.com>
2023-02-13 17:41:40 -0500

    week6 Task3 is started

--- src/week6/Task2_Array_FindMaximum.java
+++ src/week6/Task2_Array_FindMaximum.java
@@ -5,4 +5,20 @@ public class Task2_Array_FindMaximum {
     Array -- Find Maximum
 Write a method that can find the maximum number from an int Array
      */
+    public static void main(String[] args) {
+        int[] array = {5,8,9,15,65,-87,-3335,9758};
+        System.out.println(maxNumber(array));
+    }
+
+    public static int maxNumber(int[] array){
+        int max = array[0];
+
+        for (int each : array) {
+            if(each>max){
+                max = each;
+            }
+        }
+        return max;
+    }
+
 }
---
+++ src/week6/Task3_String_SumOfDigitsInString.java
@@ -0,0 +1,8 @@
+package week6;
+
+public class Task3_String_SumOfDigitsInString {
+    /*
+    String -- sum of digits in a string
+  Write a method that can return the sum of the digits in a string
+     */
+}
