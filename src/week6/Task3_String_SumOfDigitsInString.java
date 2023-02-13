f873e910f960da0d1ef894c45e5865205056aaf6
Mariia_Cydeo <mashkenciy@gmail.com>
2023-02-13 17:45:07 -0500

    week6 Task3 is complete

--- src/week6/Task3_String_SumOfDigitsInString.java
+++ src/week6/Task3_String_SumOfDigitsInString.java
@@ -5,4 +5,19 @@ public class Task3_String_SumOfDigitsInString {
     String -- sum of digits in a string
   Write a method that can return the sum of the digits in a string
      */
+
+    public static void main(String[] args) {
+        System.out.println(sumOfDigits("HSNSAAQ45SHAJ12SAHBGD"));
+    }
+    
+    public static int sumOfDigits(String str){
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if(Character.isDigit(str.charAt(i))){
+                sum += Integer.parseInt(""+str.charAt(i));
+            }
+        }
+        return sum;
+    }
+    
 }
