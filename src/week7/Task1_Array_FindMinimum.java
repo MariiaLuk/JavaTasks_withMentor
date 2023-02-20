1a582bd13491ff8c100ffccec4b04fbad4a904f2
Mariia_Cydeo <mashkenciy@gmail.com>
2023-02-20 15:16:46 -0500

    Task 1 is finished

--- src/week7/Task1_Array_FindMinimum.java
+++ src/week7/Task1_Array_FindMinimum.java
@@ -1,8 +1,34 @@
 package week7;
 
+import java.util.Arrays;
+
 public class Task1_Array_FindMinimum {
     /*
     Question1: Array - Find Minimum
-Write a method that can find the maximum number from an int Array
+    Write a method that can find the minimum number from an int Array
      */
+
+    public static void main(String[] args) {
+        int[] array = {5,78,96,-5,756,39};
+        System.out.println(findMinimum(array));
+        System.out.println(findMin_ArraysMethod(array));
+    }
+
+
+    public static int findMinimum(int[] array){
+        int min = array[0];
+
+        for (int each : array) {
+            if(each<min){
+                min = each;
+            }
+        }
+        return min;
+    }
+
+    public static int findMin_ArraysMethod (int[] array){
+        Arrays.sort(array);
+        return array[0];
+    }
+
 }
