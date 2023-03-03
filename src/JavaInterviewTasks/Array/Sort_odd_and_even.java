b9bc82073c7ab1a421452c919439fd34da24a0ff
Mariia_Cydeo <mashkenciy@gmail.com>
2023-03-03 08:47:35 -0500

    finished Java task with odd and even

--- src/JavaInterviewTasks/Array/Sort_odd_and_even.java
+++ src/JavaInterviewTasks/Array/Sort_odd_and_even.java
@@ -1,4 +1,32 @@
 package JavaInterviewTasks.Array;
 
+import java.util.Arrays;
+
 public class Sort_odd_and_even {
+
+    public static void main(String[] args) {
+
+        int[] array = {6,26,19,53,42,6,13,44};
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] % 2 != 0){
+                for (int j = i+1; j < array.length; j++) {
+                    int temp = 0;
+                    if(array[j] % 2 ==0){
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j]= temp;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+
+
+
+    }
+
+
 }
